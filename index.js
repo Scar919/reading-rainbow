3401cd6622e216d1e7ba6c2f00e9b7aaebb52833
Scarlett Borroel <scarlett@Scarletts-MacBook-Pro.local>
2023-04-19 20:09:12 -0700

    added index.js

---
+++ index.js
@@ -0,0 +1,67 @@
+
+const fs = require("fs");
+const inqu= require("inquirer");
+const generateMarkdown = require('./generateMarkdown');
+
+let questionsList = [
+  
+    {
+        type: "input",
+        name: "Title",
+        message: "What is the title of your project",
+    },
+    {
+        type: "input",
+        name: "Description",
+        message: "Please provide a short description of this project",
+    },
+    {
+        type: "input",
+        name: "Table of Contents",
+        message: "Add table of content if necessary",
+    },
+    {
+        type: "input",
+        name: "Installation",
+        message: "What are some steps for Installation?",
+    },
+    {
+        type: "input",
+        name: "Usage",
+        message: "Please provide instuctions and examples for use.",
+    },
+    {
+        type: "input",
+        name: "License",
+        message: "What type of license was chosen for your project?",
+        choices: ["Apache License 2.0", "Mozilla Public License 2.0", "MIT License", "No License"],
+    },
+    {
+        type: "input",
+        name: "Contributing",
+        message: "Please provide information on how other can contribute",
+    },
+    {
+        type: "input",
+        name: "Tests",
+        message: "Provide a test/tests for your application and provide examples on how to run them",
+    },
+    {
+        type: "input",
+        name: "Github",
+        message: "What is your github username?", 
+    },
+    {
+        type: "input",
+        name: "Email",
+        message: "what is your email?",
+    },
+
+]
+
+function init() {
+    inqu.prompt(questions).then((data) => {
+        console.log(data)
+        fs.writeFile("GeneratedREADME/GeneratedREADME.md", generateMarkdown(data), (err) => err ? console.error(err) : console.log("Success!"))})
+    }
+init();
